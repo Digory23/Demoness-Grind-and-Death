194b552ad39bcb991dc1a10741b9665d1ad1068d
nath <54672324+ndcaz12@users.noreply.github.com>
2020-12-14 04:39:27 -0500

    Creacion de nivel completado

---
+++ Demoness Grind and Death (Semestral)/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void ReloadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
--- Demoness Grind and Death (Semestral)/Assets/Scripts/ScoringSystem.cs
+++ Demoness Grind and Death (Semestral)/Assets/Scripts/ScoringSystem.cs
@@ -6,11 +6,21 @@ using UnityEngine.UI;
 public class ScoringSystem : MonoBehaviour
 {
     public GameObject objetoText;
+    public GameObject Panel;
     public static int objetosColect;
 
     void Update()
     {
         objetoText.GetComponent<Text>().text = objetosColect + " / 5";
+        /*if (objetosColect >= 5)
+        {
+            Panel.gameObject.SetActive(true);
+            //Application.LoadLevel("Nivel Completado");
+        } else
+        {
+            Panel.gameObject.SetActive(false);
+        }*/
+            
     }
 
 
