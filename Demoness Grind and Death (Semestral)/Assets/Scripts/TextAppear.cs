d40a27742984159f535ea2736abff401a70f7934
nath <54672324+ndcaz12@users.noreply.github.com>
2020-12-14 23:52:03 -0500

    texto aparece

---
+++ Demoness Grind and Death (Semestral)/Assets/Scripts/TextAppear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAppear : MonoBehaviour
+{
+    public Text canvasText1;
+
+    void Start()
+    {
+        Invoke("DisableText", 5f);//invoke after 5 seconds
+    }
+    void DisableText()
+    {
+        canvasText1.enabled = false;
+    }
+
+}
