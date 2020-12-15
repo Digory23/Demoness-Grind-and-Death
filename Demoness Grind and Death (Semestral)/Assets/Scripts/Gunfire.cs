da9c906fdc8743a63062358756a67990b0c7eea4
LordDigory <danyd2339@gmail.com>
2020-12-14 22:53:19 -0500

    ver. final 2

--- Demoness Grind and Death (Semestral)/Assets/Scripts/Gunfire.cs
+++ Demoness Grind and Death (Semestral)/Assets/Scripts/Gunfire.cs
@@ -20,25 +20,5 @@ public class Gunfire : MonoBehaviour
     }
 
 
-    //AudioSource gunsound;
-
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            gunsound = GetComponent<AudioSource>();
-            gunsound.Play();
-            //GetComponent<Animation>().Play("GunAnimation");
-        }
-    }*/
-
-
 
 }
